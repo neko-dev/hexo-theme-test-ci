a271460751ad79d0d1a6f4714fa7e34fffa6679e
neoFelhz <neofelhz@gmail.com>
2017-07-09 00:45:01 +0800

    remove: public.sh

--- public.sh
+++
@@ -1,8 +0,0 @@
-cd ./public
-git init
-git config --global push.default matching
-git config --global user.email "${GitHubEmail}"
-git config --global user.name "${GitHubName}"
-git add --all .
-git commit -m "Auto Tester of Material Theme"
-#git push --quiet --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git master
