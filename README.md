af222df079b345ed4902657fb4117b1200be43f1
neoFelhz <neofelhz@gmail.com>
2017-07-17 17:19:43 +0800

    feat/docs: update readme & improve the deploy - update the README about ENV config - Improve the deploy to specified branch - Change a ENV name

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 - [主题官网](https://material.viosey.com)
 - [主题 GitHub](https://github.com/viosey/hexo-theme-material)
 - [主题 Demo(Viosey's Blog)](https://blog.viosey.com)
+
+
+[![Version](https://img.shields.io/badge/version-1.4.0-green.svg?style=flat-square)]()
+
+[![Hexo](https://img.shields.io/badge/hexo-3.0+-green.svg?style=flat-square)](https://hexo.io)
+[![node.js](https://img.shields.io/badge/node.js-6.0%2B-green.svg?style=flat-square)](https://nodejs.org/)
+
+[![Build Status](https://img.shields.io/travis/neko-dev/hexo-theme-test-ci.svg?style=flat-square)](https://travis-ci.org/neko-dev/hexo-theme-test-ci)
+
+## Travis ENV
+
+- GitHubEmail : The E-mail for GitHub.
+- GitHubUser : The username of GitHub.
+- GitHubKEY : The `Personal Access Token` for GitHub.
+- GitHubRepo : The repo for the deployment. Format example `nekodev/nekodev.github.io` .
+- DeployBranch : Specify the branch for deployment at `${GitHubRepo}` .
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
-cd ./public
+mkdir ./deploy-git
+cd ./deploy-git
 git init
 git config --global push.default matching
 git config --global user.email "${GitHubEmail}"
-git config --global user.name "${GitHubName}"
+git config --global user.name "${GitHubUser}"
+git remote add origin https://${GitHubKEY}@github.com/${GitHubRepo}.git
+git pull origin ${DeployBranch}
+rm -rf ./*
+cp -rf ../public/* ./
 git add --all .
-git commit -m "Auto Tester of Material Theme"
-#git push --quiet --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git master
+
+if [ "$HOST_OS" = "Linux" ] || [[ "$HOST_OS" =~ "MINGW64_NT" ]] || [[ "$HOST_OS" =~ "MINGW32_NT" ]]; then
+    DATE="$(echo $(date --rfc-2822)|sed -n -e 's/\+0800/CST/p')"
+elif [ "$HOST_OS" = "Darwin" ]; then
+    DATE="$(echo $(date +'%a, %d %b %Y %T CST'))"
+fi
+
+git commit -m "Travis Auto Tester for Material Theme $DATE"
+git push --quiet --force https://${GitHubKEY}@github.com/${GitHubRepo}.git
