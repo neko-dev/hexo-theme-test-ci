caf493c721740dd5c3348253e2e55546c72b0f42
neoFelhz <neofelhz@gmail.com>
2017-07-17 17:22:54 +0800

    chore(travis): minor changes at process

---
+++ install-material.sh
@@ -0,0 +1,12 @@
+cd _hexo
+npm install
+git clone https://github.com/viosey/hexo-theme-material.git themes/material
+cd themes/material
+git checkout -t origin/canary
+git pull
+rm -rf .git
+cd ..
+cd ..
+cd ..
+cp -i _config.theme.yml _hexo/themes/material/_config.yml
+cd _hexo
